0750214d26ef7e89dafa7a44a026fa807b11f178
Tyler Sommer <sommertm@gmail.com>
2015-12-22 15:40:25 -0700

    Converted parse errors to more idiomatic implementation

--- parse/error.go
+++ parse/error.go
@@ -4,17 +4,31 @@ import (
 	"fmt"
 )
 
-// newParseError generates a generic error when no additional context
-// is available other than the current token.
-func newParseError(tok token) error {
-	return fmt.Errorf("parse error: unexpected token \"%s\" on line %d, offset %d", tok, tok.line, tok.offset)
+// ParseError represents a generic error during parsing when no additional
+// context is available other than the current token.
+type ParseError struct {
+	tok token
 }
 
-// newUnexpectedTokenError generates an error describing when one or more known
-// token types are expected and not received.
-func newUnexpectedTokenError(actual token, expected ...tokenType) error {
+func (e *ParseError) Error() string {
+	return fmt.Sprintf("parse error: unexpected token \"%s\" on line %d, offset %d", e.tok, e.tok.line, e.tok.offset)
+}
+
+// newParseError returns a new ParseError.
+func newParseError(tok token) error {
+	return &ParseError{tok}
+}
+
+// UnexpectedTokenError is generated when the current token
+// is not of the expected type.
+type UnexpectedTokenError struct {
+	actual token
+	expected []tokenType
+}
+
+func (e *UnexpectedTokenError) Error() string {
 	var s = "["
-	for i, e := range expected {
+	for i, e := range e.expected {
 		if i > 0 {
 			s = s + ", "
 		}
@@ -24,21 +38,54 @@ func newUnexpectedTokenError(actual token, expected ...tokenType) error {
 
 	s = s + "]"
 
-	return fmt.Errorf("parse error: expected one of %s, got \"%s\" on line %d, offset %d", s, actual.tokenType, actual.line, actual.offset)
+	return fmt.Sprintf("parse error: expected one of %s, got \"%s\" on line %d, offset %d", s, e.actual.tokenType, e.actual.line, e.actual.offset)
 }
 
-// newUnclosedTagError generates an error describing an unclosed tag.
+// newUnexpectedTokenError returns a new UnexpectedTokenError
+func newUnexpectedTokenError(actual token, expected ...tokenType) error {
+	return &UnexpectedTokenError{actual, expected}
+}
+
+// UnclosedTagError is generated when a tag is not properly closed.
+type UnclosedTagError struct {
+	name string
+	start pos
+}
+
+func (e *UnclosedTagError) Error() string {
+	return fmt.Sprintf("parse error: unclosed tag \"%s\" starting on line %d, offset %d", e.name, e.start.Line, e.start.Offset)
+}
+
+// newUnclosedTagError returns a new UnclosedTagError.
 func newUnclosedTagError(name string, start pos) error {
-	return fmt.Errorf("parse error: unclosed tag \"%s\" starting on line %d, offset %d", name, start.Line, start.Offset)
+	return &UnclosedTagError{name, start}
 }
 
-// newUnexpectedEofError generates an error describing an unexpected end of input.
+// UnexpectedEofError describes an unexpected end of input.
+type UnexpectedEofError struct {
+	tok token
+}
+
+func (e *UnexpectedEofError) Error() string {
+	return fmt.Sprintf("parse error: unexpected end of input on line %d, offset %d", e.tok.line, e.tok.offset)
+}
+
+// newUnexpectedEofError returns a new UnexpectedEofError
 func newUnexpectedEofError(tok token) error {
-	return fmt.Errorf("parse error: unexpected end of input on line %d, offset %d", tok.line, tok.offset)
+	return &UnexpectedEofError{tok}
 }
 
-// newUnexpectedPunctuationError generates an error describing
-// an incorrect punctuation token.
+// UnexpectedPunctuationError describes an invalid or unexpected punctuation token.
+type UnexpectedPunctuationError struct {
+	tok token
+	expected string
+}
+
+func (e *UnexpectedPunctuationError) Error() string {
+	return fmt.Sprintf("parse error: unexpected punctuation \"%s\", expected \"%s\" on line %d, offset %d", e.tok.value, e.expected, e.tok.line, e.tok.offset)
+}
+
+// newUnexpectedPunctuationError returns a new UnexpectedPunctuationError
 func newUnexpectedPunctuationError(tok token, expected string) error {
-	return fmt.Errorf("parse error: unexpected punctuation \"%s\", expected \"%s\" on line %d, offset %d", tok.value, expected, tok.line, tok.offset)
+	return &UnexpectedPunctuationError{tok, expected}
 }
