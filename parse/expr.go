95aee2030ebc9892aa46442d0cde256de7d4bb64
Tyler Sommer <sommertm@gmail.com>
2015-12-28 23:17:04 -0700

    Removed useless nodeType definitions

--- parse/expr.go
+++ parse/expr.go
@@ -6,21 +6,14 @@ type Expr interface {
 	Node
 }
 
-const (
-	exprName nodeType = iota
-	exprString
-	exprFunc
-)
-
 // NameExpr represents an identifier, such as a variable.
 type NameExpr struct {
-	nodeType
 	pos
 	name string
 }
 
 func newNameExpr(name string, pos pos) *NameExpr {
-	return &NameExpr{exprName, pos, name}
+	return &NameExpr{pos, name}
 }
 
 func (exp *NameExpr) Name() string {
@@ -33,13 +26,12 @@ func (exp *NameExpr) String() string {
 
 // StringExpr represents a string literal.
 type StringExpr struct {
-	nodeType
 	pos
 	text string
 }
 
 func newStringExpr(text string, pos pos) *StringExpr {
-	return &StringExpr{exprString, pos, text}
+	return &StringExpr{pos, text}
 }
 
 func (exp *StringExpr) String() string {
@@ -48,14 +40,13 @@ func (exp *StringExpr) String() string {
 
 // FuncExpr represents a function call.
 type FuncExpr struct {
-	nodeType
 	pos
 	name *NameExpr
 	args []Expr
 }
 
 func newFuncExpr(name *NameExpr, args []Expr, pos pos) *FuncExpr {
-	return &FuncExpr{exprFunc, pos, name, args}
+	return &FuncExpr{pos, name, args}
 }
 
 func (exp *FuncExpr) String() string {
--- parse/node.go
+++ parse/node.go
@@ -4,15 +4,8 @@ import "fmt"
 
 // A node is an item in the AST
 type Node interface {
-	Type() nodeType
 	Pos() pos
 	String() string
-}
-
-type nodeType int
-
-func (t nodeType) Type() nodeType {
-	return t
 }
 
 type pos struct {
@@ -32,25 +25,15 @@ func (p pos) String() string {
 	return fmt.Sprintf("%d:%d", p.Line, p.Offset)
 }
 
-const (
-	nodeText nodeType = iota
-	nodeModule
-	nodePrint
-	nodeBlock
-	nodeIf
-	nodeExtends
-)
-
 // ModuleNode represents a set of nodes.
 type ModuleNode struct {
-	nodeType
 	pos
 	parent *Node
 	nodes  []Node
 }
 
 func newModuleNode(nodes ...Node) *ModuleNode {
-	return &ModuleNode{nodeModule, newPos(1, 0), nil, nodes}
+	return &ModuleNode{newPos(1, 0), nil, nodes}
 }
 
 func (l *ModuleNode) append(n Node) {
@@ -67,13 +50,12 @@ func (l *ModuleNode) Children() []Node {
 
 // TextNode represents raw, non Stick source code, like plain HTML.
 type TextNode struct {
-	nodeType
 	pos
 	data string
 }
 
 func newTextNode(data string, p pos) *TextNode {
-	return &TextNode{nodeText, p, data}
+	return &TextNode{p, data}
 }
 
 func (t *TextNode) String() string {
@@ -86,13 +68,12 @@ func (t *TextNode) Text() string {
 
 // PrintNode represents a print statement
 type PrintNode struct {
-	nodeType
 	pos
 	exp Expr
 }
 
 func newPrintNode(exp Expr, p pos) *PrintNode {
-	return &PrintNode{nodePrint, p, exp}
+	return &PrintNode{p, exp}
 }
 
 func (t *PrintNode) Expr() Expr {
@@ -105,14 +86,13 @@ func (t *PrintNode) String() string {
 
 // BlockNode represents a block statement
 type BlockNode struct {
-	nodeType
 	pos
 	name string
 	body Node
 }
 
 func newBlockNode(name string, body Node, p pos) *BlockNode {
-	return &BlockNode{nodeBlock, p, name, body}
+	return &BlockNode{p, name, body}
 }
 
 func (t *BlockNode) String() string {
@@ -121,7 +101,6 @@ func (t *BlockNode) String() string {
 
 // IfNode represents an if statement
 type IfNode struct {
-	nodeType
 	pos
 	cond Expr
 	body Node
@@ -129,7 +108,7 @@ type IfNode struct {
 }
 
 func newIfNode(cond Expr, body Node, els Node, p pos) *IfNode {
-	return &IfNode{nodeIf, p, cond, body, els}
+	return &IfNode{p, cond, body, els}
 }
 
 func (t *IfNode) String() string {
@@ -138,13 +117,12 @@ func (t *IfNode) String() string {
 
 // ExtendsNode represents an extends statement
 type ExtendsNode struct {
-	nodeType
 	pos
 	tplRef Expr
 }
 
 func newExtendsNode(tplRef Expr, p pos) *ExtendsNode {
-	return &ExtendsNode{nodeExtends, p, tplRef}
+	return &ExtendsNode{p, tplRef}
 }
 
 func (t *ExtendsNode) String() string {
