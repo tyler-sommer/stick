5c9a423e24e934346e920947ab74b7b5eb5f86e2
Jason Playne <jason@jasonplayne.com>
2016-05-08 20:13:49 +0800

    more skeleton work

--- filter_test.go
+++ filter_test.go
@@ -5,27 +5,43 @@ import (
 	"bytes"
 )
 
-func TestFilterDefault(t *testing.T) {
-	env := NewEnv(nil)
+func TestFilters(t *testing.T) {
 
-	if _, ok := env.Filters["default"]; !ok {
-		t.Errorf("There is no filter named default!")
+	type test struct {
+		test, expected string
+		data           map[string]Value
+	}
+
+	values := []test{
+		{test:"Hi {{ name|default('person') }}", expected: "Hi person", data:nil},
+		{test:"{{ num|abs }}", expected:"5", data:map[string]Value{"num":-5}},
+		{test:"{{ num|abs }}", expected:"6", data:map[string]Value{"num":6}},
+		{test:"{{ pi|abs }}", expected:"3.14", data:map[string]Value{"pi":3.14}},
+		{test:"{{ pi|abs }}", expected:"3.14", data:map[string]Value{"pi":-3.14}},
+		{test:"{{ name|capitalize }}", expected:"Mr ed", data:map[string]Value{"name":"MR ED"}},
+		{test:"{{ name|lower }}", expected:"mr ed", data:map[string]Value{"name":"MR ED"}},
+		{test:"{{ name|title }}", expected:"Mr Ed", data:map[string]Value{"name":"mr ed"}},
+		{test:"{{ name|trim }}", expected:"mr ed", data:map[string]Value{"name":" mr ed "}},
+		{test:"{{ name|upper }}", expected:"MR ED", data:map[string]Value{"name":"mr ed"}},
 	}
 
 	var out string
-	var expected string = "Hi person"
 	buf := bytes.NewBufferString(out)
 
-	m := make(map[string]Value)
+	env := NewEnv(nil)
+	for _, test := range values {
+		buf.Reset()
+		t.Logf("Testing '%s' and expecting to get '%s'", test.test, test.expected)
+		out = ""
+		if err := env.Execute(test.test, buf, test.data); err != nil {
+			t.Error("Failed to Execute Template", err)
+			continue
+		}
 
-	err := env.Execute("Hi {{ name|default('person') }}", buf, m)
+		if test.expected != buf.String() {
+			t.Errorf("Failed. Expected '%s', got '%s'", test.expected, buf.String())
+			continue
+		}
 
-	if nil != err {
-		t.Error(err)
 	}
-
-	if expected != buf.String() {
-		t.Errorf("Failed to fill in default value for a variable. Expected '%s', got '%s'", expected, out)
-	}
-
 }
