16e7dffed02bd6566b3bb30ca254dd05383ea430
Alessandro Pellizzari <alex@amiran.it>
2020-10-03 08:35:31 +0100

    Add reverse filter

--- twig/filter/filter.go
+++ twig/filter/filter.go
@@ -138,53 +138,53 @@ func filterDate(ctx stick.Context, val stick.Value, args ...stick.Value) stick.V
 
 	// build a golang date string
 	table := map[string]string{
-		"d" : "02",
-		"D" : "Mon",
-		"j" : "2",
-		"l" : "Monday",
-		"N" : "", // TODO: ISO-8601 numeric representation of the day of the week (added in PHP 5.1.0)
-		"S" : "", // TODO: English ordinal suffix for the day of the month, 2 characters
-		"w" : "", // TODO: Numeric representation of the day of the week
-		"z" : "", // TODO: The day of the year (starting from 0)
-		"W" : "", // TODO: ISO-8601 week number of year, weeks starting on Monday (added in PHP 4.1.0)
-		"F" : "January",
-		"m" : "01",
-		"M" : "Jan",
-		"n" : "1",
-		"t" : "", // TODO: Number of days in the given month
-		"L" : "", // TODO: Whether it's a leap year
-		"o" : "", // TODO: ISO-8601 year number. This has the same value as Y, except that if the ISO week number (W) belongs to the previous or next year, that year is used instead. (added in PHP 5.1.0)
-		"Y" : "2006",
-		"y" : "06",
-		"a" : "pm",
-		"A" : "PM",
-		"B" : "", // TODO: Swatch Internet time (is this even still a thing?!)
-		"g" : "3",
-		"G" : "15",
-		"h" : "03",
-		"H" : "15",
-		"i" : "04",
-		"s" : "05",
-		"u" : "000000",
-		"e" : "", // TODO: Timezone identifier (added in PHP 5.1.0)
-		"I" : "", // TODO: Whether or not the date is in daylight saving time
-		"O" : "-0700",
-		"P" : "-07:00",
-		"T" : "MST",
-		"c" : "2006-01-02T15:04:05-07:00",
-		"r" : "Mon, 02 Jan 2006 15:04:05 -0700",
-		"U" : "", // TODO: Seconds since the Unix Epoch (January 1 1970 00:00:00 GMT)
+		"d": "02",
+		"D": "Mon",
+		"j": "2",
+		"l": "Monday",
+		"N": "", // TODO: ISO-8601 numeric representation of the day of the week (added in PHP 5.1.0)
+		"S": "", // TODO: English ordinal suffix for the day of the month, 2 characters
+		"w": "", // TODO: Numeric representation of the day of the week
+		"z": "", // TODO: The day of the year (starting from 0)
+		"W": "", // TODO: ISO-8601 week number of year, weeks starting on Monday (added in PHP 4.1.0)
+		"F": "January",
+		"m": "01",
+		"M": "Jan",
+		"n": "1",
+		"t": "", // TODO: Number of days in the given month
+		"L": "", // TODO: Whether it's a leap year
+		"o": "", // TODO: ISO-8601 year number. This has the same value as Y, except that if the ISO week number (W) belongs to the previous or next year, that year is used instead. (added in PHP 5.1.0)
+		"Y": "2006",
+		"y": "06",
+		"a": "pm",
+		"A": "PM",
+		"B": "", // TODO: Swatch Internet time (is this even still a thing?!)
+		"g": "3",
+		"G": "15",
+		"h": "03",
+		"H": "15",
+		"i": "04",
+		"s": "05",
+		"u": "000000",
+		"e": "", // TODO: Timezone identifier (added in PHP 5.1.0)
+		"I": "", // TODO: Whether or not the date is in daylight saving time
+		"O": "-0700",
+		"P": "-07:00",
+		"T": "MST",
+		"c": "2006-01-02T15:04:05-07:00",
+		"r": "Mon, 02 Jan 2006 15:04:05 -0700",
+		"U": "", // TODO: Seconds since the Unix Epoch (January 1 1970 00:00:00 GMT)
 	}
 	var layout string
 
-	maxLen := len(requestedLayout);
+	maxLen := len(requestedLayout)
 	for i := 0; i < maxLen; i++ {
 		char := string(requestedLayout[i])
 		if t, ok := table[char]; ok {
 			layout += t
 			continue
 		}
-		if "\\" == char && i < maxLen-1{
+		if "\\" == char && i < maxLen-1 {
 			layout += string(requestedLayout[i+1])
 			continue
 		}
@@ -236,7 +236,7 @@ func filterFormat(ctx stick.Context, val stick.Value, args ...stick.Value) stick
 }
 
 func filterJoin(ctx stick.Context, val stick.Value, args ...stick.Value) stick.Value {
-	if ! stick.IsIterable(val) {
+	if !stick.IsIterable(val) {
 		return nil
 	}
 
@@ -285,7 +285,7 @@ func filterLower(ctx stick.Context, val stick.Value, args ...stick.Value) stick.
 }
 
 func filterMerge(ctx stick.Context, val stick.Value, args ...stick.Value) stick.Value {
-	if ! stick.IsIterable(val) {
+	if !stick.IsIterable(val) {
 		return nil
 	}
 
@@ -329,8 +329,28 @@ func filterReplace(ctx stick.Context, val stick.Value, args ...stick.Value) stic
 }
 
 func filterReverse(ctx stick.Context, val stick.Value, args ...stick.Value) stick.Value {
-	// TODO: Implement Me
-	return val
+	if stick.IsArray(val) {
+		arr := reflect.ValueOf(val)
+		res := make([]interface{}, 0)
+		for i := arr.Len() - 1; i >= 0; i-- {
+			res = append(res, arr.Index(i).Interface())
+		}
+		return res
+	}
+
+	if stick.IsMap(val) {
+		return val
+	}
+
+	if s := stick.CoerceString(val); s != "" {
+		runes := []rune(s)
+		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+			runes[i], runes[j] = runes[j], runes[i]
+		}
+		return string(runes)
+	}
+
+	return nil
 }
 
 func filterRound(ctx stick.Context, val stick.Value, args ...stick.Value) stick.Value {
--- twig/filter/filter_test.go
+++ twig/filter/filter_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 
 	"github.com/tyler-sommer/stick"
-	"time"
 	"strings"
+	"time"
 )
 
 func TestFilters(t *testing.T) {
@@ -56,14 +56,19 @@ func TestFilters(t *testing.T) {
 		{"batch full", newBatchFunc([]int{1, 2, 3, 4}, 2), "1.2..3.4.."},
 		{"batch empty", newBatchFunc([]int{}, 10), ""},
 		{"batch nil", newBatchFunc(nil, 10), ""},
-		{"first array", func() stick.Value { return filterFirst(nil, []string{"1","2","3","4"}) }, "1"},
+		{"first array", func() stick.Value { return filterFirst(nil, []string{"1", "2", "3", "4"}) }, "1"},
 		{"first string", func() stick.Value { return filterFirst(nil, "1234") }, "1"},
 		{"date c", func() stick.Value { return filterDate(nil, testDate, "c") }, "1980-05-31T22:01:00+08:00"},
 		{"date r", func() stick.Value { return filterDate(nil, testDate, "r") }, "Sat, 31 May 1980 22:01:00 +0800"},
-		{"date test", func() stick.Value { return filterDate(nil, testDate2, "d D j l F m M n Y y a A g G h H i s O P T")}, "03 Sat 3 Saturday February 02 Feb 2 2018 18 am AM 2 02 02 02 01 44 +0800 +08:00 AWST"},
+		{"date test", func() stick.Value { return filterDate(nil, testDate2, "d D j l F m M n Y y a A g G h H i s O P T") }, "03 Sat 3 Saturday February 02 Feb 2 2018 18 am AM 2 02 02 02 01 44 +0800 +08:00 AWST"},
 		{"date u", func() stick.Value { return filterDate(nil, testDate2, "s.u") }, "44.123456"},
-		{"join", func() stick.Value { return filterJoin(nil, []string{"a","b","c"}, "-") }, "a-b-c"},
-		{"merge", func() stick.Value { return stickSliceToString(filterMerge(nil, []string{"a","b"}, []string{"c", "d"})) }, "a.b.c.d"},
+		{"join", func() stick.Value { return filterJoin(nil, []string{"a", "b", "c"}, "-") }, "a-b-c"},
+		{"merge", func() stick.Value {
+			return stickSliceToString(filterMerge(nil, []string{"a", "b"}, []string{"c", "d"}))
+		}, "a.b.c.d"},
+		{"reverse array", func() stick.Value { return stickSliceToString(filterReverse(nil, []string{"1", "2", "3", "4"})) }, "4.3.2.1"},
+		{"reverse string", func() stick.Value { return filterReverse(nil, "1234") }, "4321"},
+		{"reverse string utf8", func() stick.Value { return filterReverse(nil, "東京") }, "京東"},
 	}
 	for _, test := range tests {
 		res := test.actual()
